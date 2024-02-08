b37c5bbf32c6a294f08442a201be88399fc095a1
kawakibb <kawakibk7@gmail.com>
2024-02-08 23:20:59 +0100

    update file

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,19 @@
-0-pascal_triangle FILE
+#!/usr/bin/python3
+"""
+pascal_triangle
+"""
+def pascal_triangle(n):
+    """
+         returns a list  representing
+          the Pascal triangle of a nbr
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
