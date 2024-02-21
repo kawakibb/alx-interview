047b1752d871e210822fad0da333652deb50f11d
kawakibb <kawakibk7@gmail.com>
2024-02-21 22:00:02 +0100

    task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,12 +10,12 @@ def minOperations(n):
      if not isinstance(n, int):
           return 0
 
-     op = 0
+     opr = 0
      i = 2
      while (i <= n):
           if not (n % i):
                 n = int(n / i)
-                op += i
+                opr += i
                 i = 1
           i += 1
-     return op
+     return opr
