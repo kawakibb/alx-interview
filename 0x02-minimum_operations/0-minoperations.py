ef3e47e592942de199997d3c3866f947b3d993b9
kawakibb <kawakibk7@gmail.com>
2024-02-21 21:58:52 +0100

    task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,21 @@
-min operation
+#!/usr/bin/python3
+
+""" Minimum Operations """
+
+
+def minOperations(n):
+     """
+     Your text editor can execute only two operations in this file: Copy All and Paste
+     """   
+     if not isinstance(n, int):
+          return 0
+
+     op = 0
+     i = 2
+     while (i <= n):
+          if not (n % i):
+                n = int(n / i)
+                op += i
+                i = 1
+          i += 1
+     return op
